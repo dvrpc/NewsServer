130542750c5668094124eb759281ef04de5f1366
Kris Warner <kdwarn@protonmail.com>
2021-08-04 20:31:35 +0000

    Revert some changes since 11ba

--- db/index.js
+++ db/index.js
@@ -2,11 +2,11 @@
 const sequelize = require('sequelize')
 const debug = require('debug')('sql')
 const pkg = require('../package.json')
-require('dotenv').load()
+require('dotenv').config()
 
 const name = pkg.name
 const credentials = encodeURIComponent(process.env.CREDENTIALS)
-const connectionString = `postgres://${credentials}@10.1.1.194:5432/${name}`;
+const connectionString = `postgres://${credentials}@127.0.0.1:5432/${name}`;
 
 const db = module.exports = new sequelize(connectionString, {
     logging: debug,
@@ -16,7 +16,8 @@ const db = module.exports = new sequelize(connectionString, {
 // run our model definitions after creating the jawn
 require('./models.js')
 
-// sync the db
+// sync the db. force true for now to create/recreate w/dummy data 
+    //@TODO: remember to change to force=FALSE once this is up and running
 function sync(force=false) {
     return db.sync({force})
     .then(ok => console.log('synced models to db ', connectionString))
--- server.js
+++ server.js
@@ -2,17 +2,17 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 const bodyParser = require('body-parser')
-const port = 3001
+const port = process.env.PORT || 3001;
 
 // parse the inputs
 app.use(bodyParser.json({limit: '50mb'}))
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}))
 
 // allow for CORS
-app.use(cors())
+//app.use(cors())
 
 // set up the general routing path
-app.use('/api', require('./api.js'))
+app.use('/news', require('./api.js'))
 
 app.listen(port, '0.0.0.0', () => console.log('listening on the port ', port))
 
